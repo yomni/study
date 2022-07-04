da9f9280f0788a1ec7b21ae3f85ea6b095fa1ada
Yomni <yearsheep@daum.net>
2022-07-04 21:09:01 +0900

    feat : sort 1번째 문제 해결

---
+++ src/main/java/inflearn/sector6/sort/Main.java
@@ -0,0 +1,50 @@
+package inflearn.sector6.sort;
+
+import java.util.Scanner;
+
+/**
+ * 1. 선택 정렬
+ *
+ * 설명
+ * N개이 숫자가 입력되면 오름차순으로 정렬하여 출력하는 프로그램을 작성하세요.
+ * 정렬하는 방법은 선택정렬입니다.
+ *
+ * 입력
+ * 첫 번째 줄에 자연수 N(1<=N<=100)이 주어집니다.
+ * 두 번째 줄에 N개의 자연수가 공백을 사이에 두고 입력됩니다. 각 자연수는 정수형 범위 안에 있습니다.
+ *
+ * 출력
+ * 오름차순으로 정렬된 수열을 출력합니다.
+ *
+ * input :
+ * 6
+ * 13 5 11 7 23 15
+ *
+ * output :
+ * 5 7 11 13 15 23
+ */
+public class Main {
+    public int[] solution(int n, int[] arr) {
+        // 선택정렬
+        for (int i = 0; i < n - 1; i++) {
+            int minIdx = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[minIdx]) minIdx = j;
+            }
+            int temp = arr[i];
+            arr[i] = arr[minIdx];
+            arr[minIdx] = temp;
+        }
+        return arr;
+    }
+
+    public static void main(String[] args) {
+        Main main = new Main();
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) arr[i] = sc.nextInt();
+        for (int i : main.solution(n, arr)) System.out.print(i + " ");
+    }
+}
