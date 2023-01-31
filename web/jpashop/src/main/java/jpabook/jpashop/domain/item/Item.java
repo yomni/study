b3aa0ca1c886057b1fdb12151fc5fe4e42637a6a
yomni <yearsheep@gmail.com>
2023-01-31 16:07:25 +0900

    feat: 상품 도메인 개발

--- web/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
+++ web/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -1,6 +1,7 @@
 package jpabook.jpashop.domain.item;
 
 import jpabook.jpashop.domain.Category;
+import jpabook.jpashop.exception.NotEnoughStockException;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -26,4 +27,26 @@ public abstract class Item {
             joinColumns = @JoinColumn(name = "category_id"),
             inverseJoinColumns = @JoinColumn(name = "item_id"))
     private List<Category> categories = new ArrayList<>();
+
+    /**
+     * 재고 증가
+     *
+     * @param quantity
+     */
+    public void addStock(int quantity) {
+        this.stockQuantity += quantity;
+    }
+
+    /**
+     * 재고 감소
+     *
+     * @param quantity
+     */
+    public void removeStock(int quantity) {
+        int restStock = this.stockQuantity - quantity;
+        if (restStock < 0) {
+            throw new NotEnoughStockException("need more stock");
+        }
+        this.stockQuantity = restStock;
+    }
 }
