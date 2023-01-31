596ba5413cc9dea31b74e27ec2efacea9df4ed90
yomni <yearsheep@gmail.com>
2023-01-31 14:35:42 +0900

    feat: web, jpa study, entity 매핑

---
+++ web/jpashop/src/main/java/jpabook/jpashop/domain/item/Item.java
@@ -0,0 +1,29 @@
+package jpabook.jpashop.domain.item;
+
+import jpabook.jpashop.domain.Category;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Setter
+@Getter
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "dtype")
+public abstract class Item {
+    @Id
+    @GeneratedValue
+    @Column(name = "item_id")
+    private Long id;
+    private String name;
+    private int price;
+    private int stockQuantity;
+    @ManyToMany
+    @JoinTable(name = "category_item",
+            joinColumns = @JoinColumn(name = "category_id"),
+            inverseJoinColumns = @JoinColumn(name = "item_id"))
+    private List<Category> categories = new ArrayList<>();
+}
