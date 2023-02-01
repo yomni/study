24787caa5d60d9f19dfa29fc7955d7ccecd9b1eb
yomni <yearsheep@gmail.com>
2023-02-01 22:40:15 +0900

    feat: 컴포넌트 학습

--- front-end/react-study/blog/src/App.js
+++ front-end/react-study/blog/src/App.js
@@ -3,22 +3,14 @@ import React, {useState} from "react";
 import './App.css';
 
 function App() {
-  let [글제목, 글제목변경] = useState(['남자코드 추천', '강남 우동맛집', '스프링독학']);
+  let [글제목, 글제목변경] = useState(['남자코트 추천', '강남 우동맛집', '스프링 독학']);
   let [따봉, 따봉변경] = useState(0);
 
-  function 제목바꾸기() {
-    let newArray = [...글제목];
-    newArray.sort();
-    글제목변경(newArray);
-  }
-  // TODO : router 학습
   return (
     <div className="App">
       <div className="black-nav">
         <div>개발 blog</div>
       </div>
-      <button onClick={제목바꾸기}>버튼
-      </button>
       <div className="list">
         <h3> {글제목[0]} <span onClick={() => {
           따봉변경(따봉 + 1)
@@ -36,8 +28,21 @@ function App() {
         <p>2월 17일 발행</p>
         <hr/>
       </div>
+
+      <Modal state={글제목}></Modal>
+
     </div>
   );
+}
+
+function Modal(props) {
+  return (
+    <div className="modal">
+      <h4>{props.state}</h4>
+      <p>날짜</p>
+      <p>상세내용</p>
+    </div>
+  )
 }
 
 export default App;
