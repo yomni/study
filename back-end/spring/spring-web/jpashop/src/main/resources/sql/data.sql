39181fc68fb2d367335c8b223ec17f44108424d7
yomni <yearsheep@gmail.com>
2023-02-06 18:44:32 +0900

    feat: 상품 주문, 주문목록 검색 구현

---
+++ back-end/spring/spring-web/jpashop/src/main/resources/sql/data.sql
@@ -0,0 +1,13 @@
+insert into member (city, street, zipcode, name, MEMBER_ID)
+values ('수원', '103', '111-111', 'javajigi', 1);
+insert into member (city, street, zipcode, name, MEMBER_ID)
+values ('서울', '202', '222-222', 'pobi', 2);
+insert into member (city, street, zipcode, name, MEMBER_ID)
+values ('경기광주', '206', '333-333', 'yomni', 3);
+
+insert into item (name, price, stock_quantity, author, isbn, dtype, ITEM_ID)
+values ('이펙티브 자바', 32000, 100, '조슈아', '466364375-2', 'B', 1);
+insert into item (name, price, stock_quantity, author, isbn, dtype, ITEM_ID)
+values ('TDD by Example', 28000, 200, '켄트 백', '466364375-1', 'B', 2);
+insert into item (name, price, stock_quantity, author, isbn, dtype, ITEM_ID)
+values ('자바 ORM 표준 JPA 프로그래밍', 25000, 300, '김영한', '466364375-2', 'B', 3);
