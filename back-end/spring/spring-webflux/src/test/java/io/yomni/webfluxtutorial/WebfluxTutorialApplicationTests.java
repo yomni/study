b0785c86fd162fdea841efead572b74840809e19
yomni <yearsheep@gmail.com>
2023-05-17 19:25:35 +0900

    feat: spring-webflux tutorial 작성

---
+++ back-end/spring/spring-webflux/src/test/java/io/yomni/webfluxtutorial/WebfluxTutorialApplicationTests.java
@@ -0,0 +1,13 @@
+package io.yomni.webfluxtutorial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebfluxTutorialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
