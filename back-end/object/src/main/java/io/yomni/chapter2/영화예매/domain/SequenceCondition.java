2a835c6a2c7f23e78a99d332d8c79718400bb6de
yomni <yearsheep@gmail.com>
2023-05-22 20:15:54 +0900

    feat: chapter2. 객체지향 프로그래밍 추상클래스 버전 구현

---
+++ back-end/object/src/main/java/io/yomni/chapter2/영화예매/domain/SequenceCondition.java
@@ -0,0 +1,14 @@
+package io.yomni.chapter2.영화예매.domain;
+
+public class SequenceCondition implements DiscountCondition {
+    private int sequence;
+
+    public SequenceCondition(int sequence) {
+        this.sequence = sequence;
+    }
+
+    @Override
+    public boolean isSatisfiedBy(Screening screening) {
+        return screening.isSequence(sequence);
+    }
+}
