93f0a14d1b7bca8b3a2172bf64d7abfca757c003
yomni <yearsheep@gmail.com>
2023-07-24 19:28:14 +0900

    feat: 주문조회 V4 - JPA에서 DTO로 바로 조회하는 방법
    DTO 로 바로 조회 vs Entity로 조회
    ==>  편의성 vs 재사용성 트레이드 오프 영역

--- back-end/spring/spring-web/jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
+++ back-end/spring/spring-web/jpashop/src/main/java/jpabook/jpashop/api/OrderSimpleApiController.java
@@ -1,16 +1,13 @@
 package jpabook.jpashop.api;
 
-import jpabook.jpashop.domain.Address;
 import jpabook.jpashop.domain.Order;
-import jpabook.jpashop.domain.OrderStatus;
 import jpabook.jpashop.repository.OrderRepository;
 import jpabook.jpashop.repository.OrderSearch;
-import lombok.Data;
+import jpabook.jpashop.repository.OrderSimpleQueryDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.time.LocalDateTime;
 import java.util.List;
 
 import static java.util.stream.Collectors.toList;
@@ -37,45 +34,30 @@ public class OrderSimpleApiController {
     }
 
     @GetMapping("/api/v2/simple-orders")
-    public List<SimpleOrderDto> ordersV2() {
+    public List<OrderSimpleQueryDto> ordersV2() {
         List<Order> orders = orderRepository.findAllByCriteria(new OrderSearch());
 
         // N + 1 문제 발생 --> Order : Member : Delivery = 1 : 2 : 2
-        List<SimpleOrderDto> result = orders.stream()
-                .map(SimpleOrderDto::new)
+        return orders.stream()
+                .map(e -> new OrderSimpleQueryDto(e.getId(), e.getMember().getName(), e.getOrderDate(), e.getStatus(), e.getMember().getAddress()))
                 .collect(toList());
-
-        return result;
     }
 
     /**
      * fetch join 을 활용한 n+1 문제 해결
-     * @return
      */
     @GetMapping("/api/v3/simple-orders")
-    public List<SimpleOrderDto> ordersV3() {
+    public List<OrderSimpleQueryDto> ordersV3() {
         List<Order> orders = orderRepository.findAllWithMemberDelivery();
 
-        List<SimpleOrderDto> result = orders.stream()
-                .map(SimpleOrderDto::new)
+        return orders.stream()
+//                .map(OrderSimpleQueryDto::new)
+                .map(e -> new OrderSimpleQueryDto(e.getId(), e.getMember().getName(), e.getOrderDate(), e.getStatus(), e.getMember().getAddress()))
                 .collect(toList());
-        return result;
     }
 
-    @Data
-    static class SimpleOrderDto {
-        private Long orderId;
-        private String name;
-        private LocalDateTime orderDate;
-        private OrderStatus orderStatus;
-        private Address address;
-
-        public SimpleOrderDto(Order order) {
-            this.orderId = order.getId();
-            this.name = order.getMember().getName();
-            this.orderDate = order.getOrderDate();
-            this.orderStatus = order.getStatus();
-            this.address = order.getDelivery().getAddress();
-        }
+    @GetMapping("/api/v4/simple-orders")
+    public List<OrderSimpleQueryDto> ordersV4() {
+        return orderRepository.findOrderDtos();
     }
 }
--- back-end/spring/spring-web/jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
+++ back-end/spring/spring-web/jpashop/src/main/java/jpabook/jpashop/repository/OrderRepository.java
@@ -96,4 +96,13 @@ public class OrderRepository {
         ).getResultList();
     }
 
+    public List<OrderSimpleQueryDto> findOrderDtos() {
+        return em.createQuery(
+//                        "select new jpabook.jpashop.repository.OrderSimpleQueryDto(o.id, m.name, o.status, o.orderDate, d.status, d.address)" +
+                        "select new jpabook.jpashop.repository.OrderSimpleQueryDto(o.id, m.name, o.orderDate, o.status, d.address)" +
+                                " from Order o" +
+                                " join o.member m" +
+                                " join o.delivery d", OrderSimpleQueryDto.class)
+                .getResultList();
+    }
 }
