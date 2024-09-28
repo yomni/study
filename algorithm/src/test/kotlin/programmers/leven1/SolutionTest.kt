55c5a99a6a5ff9e4be65dc4245e92ae0150983d1
yomni <yearsheep@gmail.com>
2024-09-28 11:02:06 +0900

    feat: algorithm solve

---
+++ algorithm/src/test/kotlin/programmers/leven1/SolutionTest.kt
@@ -0,0 +1,32 @@
+package programmers.leven1
+
+import io.yomni.programmers.leven1.Solution
+import org.assertj.core.api.Assertions.assertThat
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.Arguments
+import org.junit.jupiter.params.provider.MethodSource
+
+@DisplayName("SolutionTest")
+class SolutionTest {
+
+    companion object {
+        @JvmStatic
+        fun argumentsProvider() = listOf(
+            Arguments.of(12, 28),
+            Arguments.of(5, 6),
+            Arguments.of(3000, 9360),
+            Arguments.of(28, 56),
+            Arguments.of(100, 217),
+            Arguments.of(1, 1),
+            Arguments.of(36, 91)
+        )
+    }
+
+    @ParameterizedTest
+    @MethodSource("argumentsProvider")
+    fun solution(input: Int, expected: Int) {
+        val solution = Solution()
+        assertThat(solution.solution(input)).isEqualTo(expected)
+    }
+}
